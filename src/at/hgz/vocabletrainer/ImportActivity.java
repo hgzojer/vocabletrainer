eb5222da2569f55c7745158ec5bab42b1dc100a3
Hans Georg ZOJER <e0226374@student.tuwien.ac.at>
2014-07-27 19:25:00 +0200

    xml import

--- src/at/hgz/vocabletrainer/ImportActivity.java
+++ src/at/hgz/vocabletrainer/ImportActivity.java
@@ -1,15 +1,58 @@
 package at.hgz.vocabletrainer;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class ImportActivity extends Activity {
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class ImportActivity extends ListActivity {
+	
+	private List<File> list = new ArrayList<File>();
+	private FileArrayAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_import);
+		
+		File dir = getExternalFilesDir(null);
+		list.clear();
+		list.addAll(Arrays.asList(dir.listFiles()));
+		adapter = new FileArrayAdapter(this, R.layout.import_item, list);
+		setListAdapter(adapter);
+	}
+	
+	private class FileArrayAdapter extends ArrayAdapter<File> {
+
+		public FileArrayAdapter(Context context, int resource,
+				List<File> objects) {
+			super(context, resource, objects);
+		}
+
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+
+	       File file = getItem(position);    
+
+	       if (convertView == null) {
+	          convertView = LayoutInflater.from(getContext()).inflate(R.layout.import_item, parent, false);
+	       }
+
+	       TextView listItemName = (TextView) convertView.findViewById(R.id.listItemName);
+	        
+	       listItemName.setText(file.getName());
+
+	       return convertView;
+	   }		
+
 	}
 }

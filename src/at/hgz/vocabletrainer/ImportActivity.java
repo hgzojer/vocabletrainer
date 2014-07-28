c70cba7c664bfa0ce0fa6abfc6c52df4b16faa41
Hans Georg ZOJER <e0226374@student.tuwien.ac.at>
2014-07-28 22:28:08 +0200

    import file dictionary name

--- src/at/hgz/vocabletrainer/ImportActivity.java
+++ src/at/hgz/vocabletrainer/ImportActivity.java
@@ -1,12 +1,15 @@
 package at.hgz.vocabletrainer;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -25,10 +28,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import at.hgz.vocabletrainer.xml.XmlUtil;
 
 public class ImportActivity extends ListActivity {
+	
+	private static class FileRow {
+		public File file;
+		public String dictionary;
+	}
 
-	private List<File> list = new ArrayList<File>();
+	private List<FileRow> list = new ArrayList<FileRow>();
 	private FileArrayAdapter adapter;
 
 	@Override
@@ -40,7 +49,6 @@ public class ImportActivity extends ListActivity {
 		list.clear();
 		File[] files = dir.listFiles(new FilenameFilter() {
 			private Pattern p = Pattern.compile("^.*\\.vt$");
-
 			@Override
 			public boolean accept(File dir, String filename) {
 				return p.matcher(filename.toLowerCase(Locale.US)).matches();
@@ -50,12 +58,24 @@ public class ImportActivity extends ListActivity {
 		if (files == null) {
 			files = new File[0];
 		}
-		list.addAll(Arrays.asList(files));
+		for (File file : files) {
+			FileRow fileRow = new FileRow();
+			fileRow.file = file;
+			fileRow.dictionary = "";
+			try {
+				InputStream in = new FileInputStream(file);
+				byte[] dictionaryBytes = IOUtils.toByteArray(in);
+				fileRow.dictionary = XmlUtil.getInstance().unmarshall(dictionaryBytes).getDictionary().getName();
+			} catch (Exception ex) {
+				// do nothing
+			}
+			list.add(fileRow);
+		}
 		adapter = new FileArrayAdapter(this, R.layout.import_item, list);
 		setListAdapter(adapter);
 	}
 
-	private void deleteFile(final File file) {
+	private void deleteFile(final FileRow fileRow) {
 
 		Resources resources = getApplicationContext().getResources();
 		String confirmDeleteDictionaryTitle = resources.getString(R.string.confirmDeleteDictionaryTitle);
@@ -67,34 +87,35 @@ public class ImportActivity extends ListActivity {
 		.setIcon(android.R.drawable.ic_dialog_alert)
 		.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 		    public void onClick(DialogInterface dialog, int whichButton) {
-				if (file.delete()) {
+				if (fileRow.file.delete()) {
 					Resources resources = getApplicationContext().getResources();
 					String text = resources.getString(R.string.deletingDictionary);
 					Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
 					toast.show();
-					adapter.remove(file);
+					adapter.remove(fileRow);
 				}
 		    }})
 		.setNegativeButton(android.R.string.no, null).show();
 	}
 	
-	private class FileArrayAdapter extends ArrayAdapter<File> {
+	private class FileArrayAdapter extends ArrayAdapter<FileRow> {
 
 		public FileArrayAdapter(Context context, int resource,
-				List<File> objects) {
+				List<FileRow> objects) {
 			super(context, resource, objects);
 		}
 
 		private class ViewHolder {
 			public ImageButton buttonDelete;
 			public TextView listItemName;
-			public File file;
+			public TextView listItemDictionary;
+			public FileRow fileRow;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 
-			File file = getItem(position);
+			FileRow fileRow = getItem(position);
 
 			if (convertView == null) {
 				convertView = LayoutInflater.from(getContext()).inflate(
@@ -102,28 +123,32 @@ public class ImportActivity extends ListActivity {
 				final ViewHolder vh = new ViewHolder();
 				vh.buttonDelete = (ImageButton) convertView.findViewById(R.id.buttonDelete);
 				vh.listItemName = (TextView) convertView.findViewById(R.id.listItemName);
+				vh.listItemDictionary = (TextView) convertView.findViewById(R.id.listItemDictionary);
 				
 				vh.buttonDelete.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						deleteFile(vh.file);
+						deleteFile(vh.fileRow);
 					}
 				});
-				vh.listItemName.setOnClickListener(new OnClickListener() {
+				OnClickListener selectFileListener = new OnClickListener() {
 					@Override
 					public void onClick(View v) {
 						Intent resultIntent = new Intent();
-						resultIntent.setData(Uri.fromFile(vh.file));
+						resultIntent.setData(Uri.fromFile(vh.fileRow.file));
 						setResult(Activity.RESULT_OK, resultIntent);
 						ImportActivity.this.finish();
 					}
-				});
+				};
+				vh.listItemName.setOnClickListener(selectFileListener);
+				vh.listItemDictionary.setOnClickListener(selectFileListener);
 				convertView.setTag(vh);
 			}
 
 			ViewHolder vh = (ViewHolder) convertView.getTag();
-			vh.file = file;
-			vh.listItemName.setText(file.getName());
+			vh.fileRow = fileRow;
+			vh.listItemName.setText(fileRow.file.getName());
+			vh.listItemDictionary.setText(fileRow.dictionary);
 
 			return convertView;
 		}

89d59f11ae8da6dbbdb1430072851564410dfe22
Hans Georg Zojer <hansgeorgzojer@Hanss-iMac.local>
2018-10-20 13:38:15 +0200

    Bugfix RuntimeException: Failure from system

--- app/src/main/java/at/hgz/vocabletrainer/AboutActivity.java
+++ app/src/main/java/at/hgz/vocabletrainer/AboutActivity.java
@@ -1,22 +1,28 @@
 package at.hgz.vocabletrainer;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
@@ -54,12 +60,40 @@ public class AboutActivity extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		
+
 		License license = list.get(position);
-		Intent intent = new Intent(AboutActivity.this, LicenseActivity.class);
-		intent.putExtra("moduleName", license.getModuleName());
-		intent.putExtra("licenseText", license.getLicenseText());
-		AboutActivity.this.startActivity(intent);
+		if (license.getLicenseText().length() > 100) {
+			Context context = AboutActivity.this;
+			try {
+				File licensePath = new File(context.getCacheDir(), "licenses");
+				if (!licensePath.exists()) {
+					if (!licensePath.mkdirs()) {
+						throw new IOException("Error creating dir: " + licensePath);
+					}
+				}
+				String fileName = license.getModuleName().replace('.', '_') + ".txt";
+				File licenseFile = new File(licensePath, fileName);
+				FileUtils.writeStringToFile(licenseFile, license.getLicenseText());
+				Uri contentUri = FileProvider.getUriForFile(context,
+						"at.hgz.vocabletrainer.fileprovider",
+						licenseFile);
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setDataAndType(contentUri, "text/plain");
+				intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+				AboutActivity.this.startActivity(intent);
+			} catch (IOException e) {
+				Log.e("VocableTrainer", "Error loading license: " + e.getMessage(), e);
+				String text = context.getResources().getString(R.string.errorLoadingLicense);
+				Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+				toast.show();
+				e.printStackTrace();
+			}
+		} else {
+			Intent intent = new Intent(AboutActivity.this, LicenseActivity.class);
+			intent.putExtra("moduleName", license.getModuleName());
+			intent.putExtra("licenseText", license.getLicenseText());
+			AboutActivity.this.startActivity(intent);
+		}
 	}
 
 	private class LicenseArrayAdapter extends ArrayAdapter<License> {
